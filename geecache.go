b82231162d984006e6c95e17361bda38ca2ba584
wendellwf <wendellwangfei@gmail.com>
2025-07-09 12:55:25 +0800

    fix: 分布式节点

--- geecache.go
+++ geecache.go
@@ -24,6 +24,7 @@ type Group struct {
 	name      string
 	getter    Getter
 	mainCache cache
+	peers     PeerPicker
 }
 
 var (
@@ -67,6 +68,14 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err = g.getFromPeer(peer, key); err != nil {
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer: ", err)
+		}
+	}
 	return g.getLocally(key)
 }
 
@@ -82,4 +91,19 @@ func (g *Group) getLocally(key string) (ByteView, error) {
 
 func (g *Group) populateCache(key string, value ByteView) {
 	g.mainCache.add(key, value)
+}
+
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
 }
